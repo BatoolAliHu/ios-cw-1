63eaeb3b419d8c940d2cb95d24608c78f6efc203
BatoolAliHussain <66515156+BatoolAliHu@users.noreply.github.com>
2020-06-13 20:55:08 +0300

    done 2
    تم الانتهاء من عمل المشروع و المعادله والعمليات الشرطية  وعمل برمجة اختبار الصداقة👍🏻

--- CW-1-batool82.playground/Contents.swift
+++ CW-1-batool82.playground/Contents.swift
@@ -4,12 +4,52 @@ var str = "Hello, playground"
 
 let name = "batool"
 
-var hoppy1 = "سباحة"
+var hoppy1 = "رسم"
 
-var hoppy2 = "رسم"
+var hoppy2 = "جمباز"
 
 var age = 17
 
 var height = 169
 
-print( name, hoppy1,  hoppy2,  age, height )
+print( "my name is:", name, "my hoppies are:", hoppy1,"-", hoppy2,"my age is:",  age, "my height is:", height )
+
+let friendName = "Alzain"
+
+let frirndHeight = 161
+
+let  friendAge = 17
+
+let friendHoppy1 = "جمباز"
+
+let friendHoppy2 = "كرة طائرة"
+
+var age_difference = age - friendAge
+
+name != friendName
+name == friendName
+
+hoppy1 != friendHoppy1
+hoppy1 == friendHoppy1
+hoppy1 != friendHoppy2
+hoppy2 != friendHoppy2
+hoppy2 == friendHoppy2
+hoppy2 != friendHoppy1
+
+height != frirndHeight
+height == frirndHeight
+height > frirndHeight
+height >= frirndHeight
+height < frirndHeight
+height <= frirndHeight
+
+let bestFriends = hoppy1 == friendHoppy1 || hoppy1 == friendHoppy2 || hoppy2 == friendHoppy1 || hoppy2 == friendHoppy2 || height == frirndHeight || age == friendAge || name == friendName
+
+
+
+
+
+
+
+
+
