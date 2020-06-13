8fbe0e58196ff8f6dc9c0cc7fcc40abf98535728
BatoolAliHussain <66515156+BatoolAliHu@users.noreply.github.com>
2020-06-13 19:02:10 +0300

    done
    تم الانتهاء من عمل التمرين

---
+++ CW-1-batool82.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "batool"
+
+var hoppy1 = "سباحة"
+
+var hoppy2 = "رسم"
+
+var age = 17
+
+var height = 169
+
+print( name, hoppy1,  hoppy2,  age, height )
